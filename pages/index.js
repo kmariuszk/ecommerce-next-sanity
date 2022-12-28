a079a7a5f9ddd4dab6244730dad1500eeb2d73c6
kmariuszk <kiciorm@gmail.com>
2022-12-28 16:30:55 +0100

    Update readme with screenshots

--- pages/index.js
+++ pages/index.js
@@ -36,7 +36,7 @@ export const getServerSideProps = async () => {
 
   const bannerQuery = '*[_type == "banner"]';
   const bannerData = await client.fetch(bannerQuery);
-
+  
   return {
     props: { products, bannerData }
   }
